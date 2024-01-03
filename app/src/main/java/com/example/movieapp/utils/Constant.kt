509ab0c1a88caaa7b8a9ef1544e7fa11a66ba416
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2024-01-03 23:45:00 +0200

    add recycler view

---
+++ app/src/main/java/com/example/movieapp/utils/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.movieapp.utils
+
+object Constant {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "c3d63be904561570038f7fb4b1cbda20"
+    const val IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
+
+}
